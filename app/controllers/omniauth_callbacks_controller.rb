3804d6a4e03aab9f27d0545c89338db195c931a7
Jason Yuen <jayuen@gmail.com>
2014-01-09 23:52:46 -0500

    redirects to users show on signin

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -10,4 +10,8 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
       redirect_to new_user_registration_url
     end
   end
+
+  def after_sign_in_path_for(resource)
+    user_path(resource)
+  end
 end
