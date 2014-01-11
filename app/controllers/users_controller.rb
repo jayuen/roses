2a7e6a6c7b37ae80a0be1c286f1829ab45433bf1
Jason Yuen <jayuen@gmail.com>
2014-01-11 01:49:18 -0500

    loads data

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,11 @@
 class UsersController < ApplicationController
   def show
+    season = Season.where(current: true).first
+    weeks = Week.where(season_id: season.id)    
+    picks = Pick.where("week_id IN (:week_ids)", week_ids: weeks.pluck(:id))
+
+    contestants = Contestant.where(season_id: season.id, eliminated: false)
+    this_week = Week.where(season_id: season.id, locked: false).first
+    eligible_contestants = EligibleContestant.where(week_id: this_week.id)
   end
 end
