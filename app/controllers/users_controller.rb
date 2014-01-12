ab1f4c613bfb755a3fcef483f305ee7f65059d72
Jason Yuen <jayuen@gmail.com>
2014-01-12 02:06:56 -0500

    maps eligible contestants to contestants

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,16 +5,6 @@ class UsersController < ApplicationController
     @weeks = Week.where(season_id: season.id)    
 
     @contestants = Contestant.where(season_id: season.id, eliminated: false)
-    @eligible_contestants = EligibleContestant.where(week_id: current_week_id)
-
-    @user = User.find(params[:id])
-    picks = Pick.where("week_id IN (:week_ids) AND user_id = :user_id", week_ids: @weeks.pluck(:id), user_id: params[:id])
-    if picks.empty?
-      picks = @eligible_contestants.map do |c|
-        Pick.create! user_id: params[:id], week_id: current_week_id
-      end
-    end
-    @user.picks = picks 
-    puts @user.picks
+    @eligible_contestants = EligibleContestant.where(week_id: current_week_id).map {|ec| ec.contestant }
   end
 end
