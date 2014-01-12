b3fed013462f2f8f1f6d9402da2070b28cff16a8
Jason Yuen <jayuen@gmail.com>
2014-01-12 02:15:31 -0500

    actually send the contestant id

--- app/assets/javascripts/roses/services.js
+++ app/assets/javascripts/roses/services.js
@@ -3,7 +3,7 @@ var services = angular.module('roses.services', [])
 services.factory('PicksRepo', ['$http', function($http) {
   return {
     save: function(pick) {
-      return $http.put('/picks/' + pick.id, {pick: {contestant_id: pick.contestant_id, rose: pick.rose}}).then(function(result) {
+      return $http.put('/picks/' + pick.id, {pick: {contestant_id: pick.contestant.id, rose: pick.rose}}).then(function(result) {
         return result.data
       })
     }
