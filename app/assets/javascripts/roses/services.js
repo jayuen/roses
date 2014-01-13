1651e4a5d337d40b92c4680a489df5d082445073
Jason Yuen <jayuen@gmail.com>
2014-01-12 21:44:58 -0500

    handles no contestant picked

--- app/assets/javascripts/roses/services.js
+++ app/assets/javascripts/roses/services.js
@@ -3,7 +3,12 @@ var services = angular.module('roses.services', [])
 services.factory('PicksRepo', ['$http', function($http) {
   return {
     save: function(pick) {
-      return $http.put('/picks/' + pick.id, {pick: {contestant_id: pick.contestant.id, rose: pick.rose}}).then(function(result) {
+      var data = { pick: { rose: pick.rose} }
+      if (pick.contestant) {
+        data.contestant_id = pick.contestant.id
+      } 
+
+      return $http.put('/picks/' + pick.id, data).then(function(result) {
         return result.data
       })
     }
