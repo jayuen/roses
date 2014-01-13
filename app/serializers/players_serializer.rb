45d110506b599f129d2265c7bd17cb0f3bb6bfdd
Jason Yuen <jayuen@gmail.com>
2014-01-12 23:55:25 -0500

    player serializes picks

--- app/serializers/players_serializer.rb
+++ app/serializers/players_serializer.rb
@@ -1,4 +1,9 @@
 class PlayersSerializer < ActiveModel::Serializer
+  class PickSerializer < ActiveModel::Serializer
+    attributes :rose_order, :rose, :week_id, :contestant, :id
+    has_one :contestant
+  end
+
   attributes :winner_id
-  has_many :picks
+  has_many :picks, serializer: PickSerializer
 end
