01f2ef89b326813991a5769aad7cf55d45e578b3
Jason Yuen <jayuen@gmail.com>
2014-01-14 21:54:58 -0500

    adds link to picks from header

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,21 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   before_filter :user_signed_in?
-  before_filter :set_season
+  before_filter :set_season, :set_player
 
   def set_season
     @current_season = Season.where(current: true).first
   end
 
+  def set_player
+    if current_user
+      @player = Player.where(season_id: @current_season.id, user_id: current_user.id).first
+    end
+  end
+
   def index
     if current_user
-      player = Player.where(season_id: @current_season.id, user_id: current_user.id).first
-      redirect_to player_path(player)
+      redirect_to player_path(@player)
     end
   end
 
