16b11fcc4b1b86aa744841fd212dc41ca7ae249f
Jason Yuen <jayuen@gmail.com>
2014-01-14 21:19:57 -0500

    adds admin capabilities

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,19 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   before_filter :user_signed_in?
+  before_filter :set_season
+
+  def set_season
+    @current_season = Season.where(current: true).first
+  end
+
+  def index
+    if current_user
+      player = Player.where(season_id: @current_season.id, user_id: current_user.id).first
+      redirect_to player_path(player)
+    end
+  end
+
+  def admin
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ Roses::Application.routes.draw do
 
   resources :players do
   end
+
+  get 'admin' => 'application#admin'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
