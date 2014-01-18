d54d25d4f93f5ab9714f298d135baebe473fed2a
Jason Yuen <jayuen@gmail.com>
2014-01-18 01:55:58 -0500

    adds spec for clearing correct picks

--- spec/services/scoring_spec.rb
+++ spec/services/scoring_spec.rb
@@ -4,10 +4,10 @@ describe "Services::Scoring" do
   let(:kelly) { Contestant.new }
   let(:sally) { Contestant.new }
   let(:molly) { Contestant.new }
+  let(:weekly_entry) { WeeklyEntry.new }
 
   describe "set_correct_picks" do
     it "set the correct picks when all picks are right" do
-      weekly_entry = WeeklyEntry.new
       weekly_entry.picks.build rose: true, contestant: sally
       weekly_result = WeeklyResult.new rose: true, contestant: sally
 
@@ -17,7 +17,6 @@ describe "Services::Scoring" do
     end
 
     it "sets the correct picks when all picks are wrong" do
-      weekly_entry = WeeklyEntry.new
       weekly_entry.picks.build rose: false, contestant: sally
       weekly_result = WeeklyResult.new rose: true, contestant: sally
 
@@ -25,11 +24,20 @@ describe "Services::Scoring" do
 
       weekly_entry.correct_picks.should == 0
     end
+
+    it "resets the correct picks" do
+      weekly_entry.correct_picks = 5
+      weekly_entry.picks.build rose: true, contestant: sally
+      weekly_result = WeeklyResult.new rose: true, contestant: sally
+
+      Scoring.set_correct_picks(weekly_entry, [weekly_result])
+
+      weekly_entry.correct_picks.should == 1
+    end
   end
 
   describe "set_final_rose_distance" do
     it "computes the final rose distance" do
-      weekly_entry = WeeklyEntry.new
       weekly_entry.picks.build rose_order: 1, rose: true, contestant: sally
       weekly_entry.picks.build rose_order: 2, rose: true, contestant: molly
       weekly_entry.picks.build rose_order: 3, rose: true, contestant: kelly
@@ -42,6 +50,14 @@ describe "Services::Scoring" do
       Scoring.set_final_rose_distance(weekly_entry, results)
 
       weekly_entry.final_rose_distance.should == -2
+    end
+
+    it "resets the final rose distance" do
+      weekly_entry.final_rose_distance = 100
+
+      Scoring.set_final_rose_distance(weekly_entry, [])
+
+      weekly_entry.final_rose_distance.should == 0
     end
   end
 end
