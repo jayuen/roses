78d1cbff5be38f28a785bc54bda7dd9677c559ce
Jason Yuen <jayuen@gmail.com>
2014-01-18 00:42:19 -0500

    autoloads services dir properly

--- config/application.rb
+++ config/application.rb
@@ -17,7 +17,7 @@ module Roses
 
     # Custom directories with classes and modules you want to be autoloadable.
     # config.autoload_paths += %W(#{config.root}/extras)
-    config.autoload_paths += %W(app/services)
+    config.autoload_paths += %W(#{config.root}/app/services)
 
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
