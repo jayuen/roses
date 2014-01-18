2b51c1795634f749a53da3a00d90dd1e76658230
Jason Yuen <jayuen@gmail.com>
2014-01-18 00:51:00 -0500

    change method under test

--- app/services/scoring.rb
+++ app/services/scoring.rb
@@ -4,6 +4,7 @@ module Scoring
     weekly_entry = week.weekly_entries.first
     weekly_results = week.weekly_results
     set_correct_picks(weekly_entry, weekly_results)
+    set_final_rose_distance(weekly_entry, weekly_results)
   end
 
   def self.set_correct_picks(weekly_entry, weekly_results)
@@ -14,5 +15,8 @@ module Scoring
         weekly_entry.correct_picks += 1
       end
     end
+  end
+
+  def self.set_final_rose_distance(weekly_entry, weekly_results)
   end
 end
--- spec/services/scoring_spec.rb
+++ spec/services/scoring_spec.rb
@@ -5,25 +5,42 @@ describe "Services::Scoring" do
   let(:sally) { Contestant.new }
   let(:molly) { Contestant.new }
 
-  it "computes the correct picks when all picks are right" do
-    week = Week.new
-    weekly_entry = week.weekly_entries.build  
-    weekly_entry.picks.build rose_order: 1, rose: true, contestant: sally
-    week.weekly_results.build rose_order: 1, rose: true, contestant: sally
+  describe "set_correct_picks" do
+    it "set the correct picks when all picks are right" do
+      weekly_entry = WeeklyEntry.new
+      weekly_entry.picks.build rose_order: 1, rose: true, contestant: sally
+      weekly_result = WeeklyResult.new rose_order: 1, rose: true, contestant: sally
 
-    Scoring.compute(week)
+      Scoring.set_correct_picks(weekly_entry, [weekly_result])
 
-    weekly_entry.correct_picks.should == 1
+      weekly_entry.correct_picks.should == 1
+    end
+
+    it "sets the correct picks when all picks are wrong" do
+      weekly_entry = WeeklyEntry.new
+      weekly_entry.picks.build rose_order: 1, rose: false, contestant: sally
+      weekly_result = WeeklyResult.new rose_order: 1, rose: true, contestant: sally
+
+      Scoring.set_correct_picks(weekly_entry, [weekly_result])
+
+      weekly_entry.correct_picks.should == 0
+    end
   end
 
-  it "computes the correct picks when all picks are wrong" do
-    week = Week.new
-    weekly_entry = week.weekly_entries.build  
-    weekly_entry.picks.build rose_order: 1, rose: false, contestant: sally
-    week.weekly_results.build rose_order: 1, rose: true, contestant: sally
+  describe "set_final_rose_distance" do
+    xit "computes the final rose distance" do
+      weekly_entry = week.weekly_entries.build  
+      weekly_entry.picks.build rose_order: 1, rose: true, contestant: sally
+      weekly_entry.picks.build rose_order: 2, rose: true, contestant: molly
+      weekly_entry.picks.build rose_order: 3, rose: true, contestant: kelly
 
-    Scoring.compute(week)
+      week.weekly_results.build rose_order: 1, rose: true, contestant: kelly
+      week.weekly_results.build rose_order: 2, rose: true, contestant: sally
+      week.weekly_results.build rose_order: 3, rose: true, contestant: molly
 
-    weekly_entry.correct_picks.should == 0
+      Scoring.compute(week)
+
+      weekly_entry.correct_picks.should == 0
+    end
   end
 end
