4fb741d9c63e07b12ebecb903bf208d360b6d978
Jason Yuen <jayuen@gmail.com>
2014-01-27 00:34:29 -0500

    adds winner pick to standings

--- app/models/player.rb
+++ app/models/player.rb
@@ -2,8 +2,13 @@
 class Player < ActiveRecord::Base
   has_many :weekly_entries
   belongs_to :user
+  belongs_to :winner, class_name: 'Contestant'
 
   def name
     user.name
+  end
+
+  def winner_name
+    winner.try(:name)
   end
 end
